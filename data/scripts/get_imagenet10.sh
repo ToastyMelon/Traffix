52942b920c49f83d890bb9e632992ea3d2021f1a
sarptandoven <119648987+sarptandoven@users.noreply.github.com>
2024-09-15 01:48:43 -0400

    Add files via upload

---
+++ data/scripts/get_imagenet10.sh
@@ -0,0 +1,19 @@
+if [ "$#" -gt 0 ]; then
+  for opt in "$@"; do
+    case "${opt}" in
+    --train) train=true ;;
+    --val) val=true ;;
+    esac
+  done
+else
+  train=true
+  val=true
+fi
+
+# Make dir
+d='../datasets/imagenet10' # unzip directory
+mkdir -p $d && cd $d
+
+# Download/unzip train
+wget https://github.com/ultralytics/yolov5/releases/download/v1.0/imagenet10.zip
+unzip imagenet10.zip && rm imagenet10.zip
---
+++ data/scripts/get_imagenet1000.sh
@@ -0,0 +1,19 @@
+if [ "$#" -gt 0 ]; then
+  for opt in "$@"; do
+    case "${opt}" in
+    --train) train=true ;;
+    --val) val=true ;;
+    esac
+  done
+else
+  train=true
+  val=true
+fi
+
+# Make dir
+d='../datasets/imagenet1000' # unzip directory
+mkdir -p $d && cd $d
+
+# Download/unzip train
+wget https://github.com/ultralytics/yolov5/releases/download/v1.0/imagenet1000.zip
+unzip imagenet1000.zip && rm imagenet1000.zip
